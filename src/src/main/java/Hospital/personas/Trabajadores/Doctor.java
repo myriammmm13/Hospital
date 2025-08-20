4a8ec28256c1ec4775d2819900caad190d971fff
myriammmm13 <161982232+myriammmm13@users.noreply.github.com>
2025-08-20 01:57:08 -0600

    Detalles Doctor y Farmaceutico

--- src/src/main/java/Hospital/personas/Trabajadores/Doctor.java
+++ src/src/main/java/Hospital/personas/Trabajadores/Doctor.java
@@ -6,7 +6,7 @@ public class Doctor extends Trabajador {
     private String Especialidad; //maybe hacer especialidad como una clase/lista para que solo la seleccionen
 
     Doctor(String nombre, String apellido1, String apellido2, String id, String Especialidad) {
-        super(nombre, apellido1, apellido2, id);
+        super(nombre, apellido1, apellido2, id);//eventualmente se debe agregar clave también, puesto que super lo necesita
         this.Especialidad = Especialidad;
     }
 
--- src/src/main/java/Hospital/personas/Trabajadores/Farmaceutico.java
+++ src/src/main/java/Hospital/personas/Trabajadores/Farmaceutico.java
@@ -1,4 +1,9 @@
 package Hospital.personas.Trabajadores;
 
-public class Farmaceutico {
+import Hospital.personas.Trabajador;
+
+public class Farmaceutico extends Trabajador {
+    Farmaceutico(String nombre, String apellido1, String apellido2, String id) {
+        super(nombre, apellido1, apellido2, id);//luego se debe agregar clave, también lo usa
+    }
 }
