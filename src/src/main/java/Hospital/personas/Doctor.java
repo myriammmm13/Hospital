a96e6d9c78d2f3ec52d90db9c6ba1f726f0e40bd
myriammmm13 <161982232+myriammmm13@users.noreply.github.com>
2025-08-19 23:58:32 -0600

    Agregación de Persona, Pacientes y Trabajador (Doctor y Farmaceutico). Falta crearles las fechas, claves y maybe cambiar la String Especialidad a ser una clase, eso para que el usuario solo escoja entre un catálogo (lista) de opciones.

---
+++ src/src/main/java/Hospital/personas/Doctor.java
@@ -0,0 +1,18 @@
+package Hospital.personas;
+
+public class Doctor extends Trabajador {
+    private String Especialidad; //maybe hacer especialidad como una clase/lista para que solo la seleccionen
+
+    Doctor(String nombre, String apellido1, String apellido2, String id, String Especialidad) {
+        super(nombre, apellido1, apellido2, id);
+        this.Especialidad = Especialidad;
+    }
+
+    public String getEspecialidad() {
+        return Especialidad;
+    }
+
+    public void setEspecialidad(String especialidad) {
+        Especialidad = especialidad;
+    }
+}
---
+++ src/src/main/java/Hospital/personas/Farmaceutico.java
@@ -0,0 +1,4 @@
+package Hospital.personas;
+
+public class Farmaceutico {
+}
