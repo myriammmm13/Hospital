1473299210fcebf1ac34e88beafa9d7ce3432ac3
Aaron-Garc <aarongarfon@gmail.com>
2025-08-20 18:43:35 -0600

    Fecha nacimiento

--- src/src/main/java/Hospital/personas/Paciente.java
+++ src/src/main/java/Hospital/personas/Paciente.java
@@ -1,13 +1,15 @@
 package Hospital.personas;
 
+import Hospital.Fecha;
+
 public class Paciente extends Persona {
     private int TelNum;
-    //private Fecha FechaNacimiento; se debe crear la clase fecha :)
+    private Fecha FechaNacimiento;
 
-    public Paciente(String nombre, String apellido1, String apellido2, String id, int TelNum) {
+    public Paciente(String nombre, String apellido1, String apellido2, String id, int TelNum, Fecha FechaNacimiento) {
         super(nombre, apellido1, apellido2, id);
         this.TelNum = TelNum;
-        //this.FechaNacimiento = FechaNacimiento; se debe agregar en el parámetro :)
+        this.FechaNacimiento = FechaNacimiento;
     }
 
     public int getTelNum() {
@@ -17,12 +19,11 @@ public class Paciente extends Persona {
     public void setTelNum(int telNum) {
         TelNum = telNum;
     }
-    /*public Fecha getFechaNacimiento(){
+    public Fecha getFechaNacimiento(){
         return FechaNacimiento;
     }
 
     public void setFechaNacimiento(Fecha FechaNacimiento){
         this.FechaNacimiento=FechaNacimiento;
     }
-    * */
 }
