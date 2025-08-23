d88bbe5ce6dbb59705ca76420fcebfa12e11b657
myriammmm13 <161982232+myriammmm13@users.noreply.github.com>
2025-08-23 15:52:52 -0600

    Agregación lista doctores, farma y pacientes

--- src/src/main/java/Hospital/data/Data.java
+++ src/src/main/java/Hospital/data/Data.java
@@ -1,18 +1,26 @@
 package Hospital.data;
 
 import Hospital.logic.medicamentos.Medicamento;
+import Hospital.logic.personas.Paciente;
 import Hospital.logic.recetas.Receta;
-
+import Hospital.logic.personas.trabajadores.Doctor;
+import Hospital.logic.personas.trabajadores.Farmaceutico;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Data {
     private List<Medicamento> medicamentos;
     private List<Receta> recetas;
+    private  List<Paciente> pacientes;
+    private  List<Doctor> doctores;
+    private List<Farmaceutico> famaceuticos;
 
     public Data(){
         medicamentos = new ArrayList<>();
         recetas = new ArrayList<>();
+        pacientes = new ArrayList<>();
+        doctores = new ArrayList<>();
+        famaceuticos = new ArrayList<>();
         medicamentos.add(new Medicamento("111", "Acetaminofen", "500mg"));
         medicamentos.add(new Medicamento("112", "Ibuprofeno", "400mg"));
         medicamentos.add(new Medicamento("113", "Amoxicilina", "250mg cápsulas"));
@@ -27,4 +35,9 @@ public class Data {
         return medicamentos;
     }
 
+    public List<Paciente> getPacientes() {return  pacientes;}
+
+    public List<Doctor> getDoctores() {return doctores;}
+
+    public List<Farmaceutico> getFamaceuticos() {}
 }
