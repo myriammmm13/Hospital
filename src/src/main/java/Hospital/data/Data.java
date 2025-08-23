c94ec80c3449bc297bc09b4b759fd3ffec1342fc
Aaron-Garc <aarongarfon@gmail.com>
2025-08-23 15:16:09 -0600

    create receta

--- src/src/main/java/Hospital/data/Data.java
+++ src/src/main/java/Hospital/data/Data.java
@@ -22,13 +22,8 @@ public class Data {
         return medicamentos;
     }
 
-
     public List<Receta> getRecetas() {
         return recetas;
-    }
-
-    public List<Medicamento> getMedicamentos() {
-        return medicamentos;
     }
 
 }
--- src/src/main/java/Hospital/logic/Service.java
+++ src/src/main/java/Hospital/logic/Service.java
@@ -2,6 +2,7 @@ package Hospital.logic;
 
 import Hospital.data.Data;
 import Hospital.logic.medicamentos.Medicamento;
+import Hospital.logic.recetas.Receta;
 
 import java.util.List;
 
@@ -67,7 +68,21 @@ public class Service {
         if (!eliminado) throw new Exception("Medicamento no encontrado para eliminar");
     }
 
+    //create recetas
+    public void agregarReceta(Receta receta, String userId) throws Exception {
+        validarRol(userId,"MED");
+        for (Receta r : data.getRecetas()) {
+            if (r.equals(receta)) {
+                throw new Exception("Ya se ha registrado esta receta");
+            }
+        }
+        data.getRecetas().add(receta);
+    }
 
+    //read recetas
+    public List<Receta> listarRecetas() {
+        return data.getRecetas();
+    }
 
 
 
