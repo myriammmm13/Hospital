23d4bce4667c12ff28250c29a12a97565f8bbc27
Aarón García <aarongarfon@gmail.com>
2025-08-28 13:59:28 -0600

    Update RecetaModel.java

--- src/src/main/java/Hospital/presentation/recetas/RecetaModel.java
+++ src/src/main/java/Hospital/presentation/recetas/RecetaModel.java
@@ -1,12 +1,16 @@
 package Hospital.presentation.recetas;
 
 import Hospital.logic.recetas.Receta;
+import Hospital.logic.Medicamento;
+import Hospital.logic.personas.Paciente;
 import Hospital.presentation.AbstractModel;
 
 import java.beans.PropertyChangeListener;
 
 public class RecetaModel extends AbstractModel {
     Receta current;
+    List<Medicamento> medicamentos;
+    List<Paciente> pacientes;
 
     public static final String CURRENT = "current";
 
