14e3e553d1795473624482e3b99f81dab930c14c
Aarón García <aarongarfon@gmail.com>
2025-08-29 09:28:35 -0600

    Update Prescripcion.java

--- src/src/main/java/Hospital/logic/recetas/Prescripcion.java
+++ src/src/main/java/Hospital/logic/recetas/Prescripcion.java
@@ -4,14 +4,35 @@ import Hospital.logic.Medicamento;
 
 public class Prescripcion {
     private Medicamento medicamento;
-    private String instrucciones;
-    private int plazo;
+    private String indicaciones;
+    private int duracion;
     private int cantidad;
 
-    public Prescripcion(Medicamento medicamento, String instrucciones, int plazo, int cantidad) {
+    public Prescripcion(Medicamento medicamento, String indicaciones, int duracion, int cantidad) {
         this.medicamento = medicamento;
-        this.instrucciones = instrucciones;
-        this.plazo = plazo;
+        this.indicaciones = indicaciones;
+        this.duracion = duracion;
         this.cantidad = cantidad;
     }
+
+    public String getNombre(){
+        return medicamento.getNombre();
+    }
+
+    public String getPresentacion(){
+        return medicamento.getPresentacion();
+    }
+
+    public int getCantidad(){
+        return cantidad;
+    }
+
+    public String getIndicaciones(){
+        return indicaciones;
+    }
+
+    public int getDuracion(){
+        return duracion;
+    }
+    
 }
