1ab6a9dbab5b17416e76337d235f68b7aef54c7a
myriammmm13 <161982232+myriammmm13@users.noreply.github.com>
2025-09-04 17:07:03 -0600

    coso

--- src/src/main/java/Hospital/Main.java
+++ src/src/main/java/Hospital/Main.java
@@ -1,8 +1,8 @@
 package Hospital;
-/*
+
 import Hospital.presentation.personas.Doctor.Controller;
 import Hospital.presentation.personas.Doctor.Model;
-import Hospital.presentation.personas.Doctor.View;*/
+import Hospital.presentation.personas.Doctor.View;
 import Hospital.presentation.personas.Administrador.Controller;
 import Hospital.presentation.personas.Administrador.Model;
 import Hospital.presentation.personas.Administrador.View;
@@ -16,7 +16,9 @@ import java.awt.*;public class Main {
     public static void main(String[] args) {
         try {UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");}
         catch (Exception ex) {};
-
+        String cod=new String();
+        //hacer todo el proceso del login
+        switch (cod[:])
         //solo tengo esto para probar, luego se debe hacer el login e iniciar con eso :)
         Hospital.presentation.personas.Administrador.View view = new Hospital.presentation.personas.Administrador.View();
         Hospital.presentation.personas.Administrador.Model model = new Hospital.presentation.personas.Administrador.Model();
@@ -25,10 +27,15 @@ import java.awt.*;public class Main {
         JFrame window = new JFrame();
         window.setSize(600,400);
         window.setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        window.setTitle("Login");
-        window.setContentPane(view.getPanel());
+        window.setTitle("Login");//se pone el cod de la persona que ingresa
+        window.setContentPane(view.getPanel()); //se le debe agregar cada tab por cada caso
         window.setLocationRelativeTo(null);
         window.setVisible(true);
+
+        Hospital.presentation.personas.Doctor.View view = new Hospital.presentation.personas.Doctor.View();
+        Hospital.presentation.personas.Doctor.Model model = new Hospital.presentation.personas.Doctor.Model();
+        Hospital.presentation.personas.Doctor.Controller controller = new Hospital.presentation.personas.Doctor.Controller(model, view);
+
     }
 
     public static final Color BACKGROUND_ERROR = new Color(255, 102, 102);
