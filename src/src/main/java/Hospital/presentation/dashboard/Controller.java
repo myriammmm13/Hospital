2533370ca48577aa3bf013c8b1708d6c3eadf09f
Aaron-Garc <aarongarfon@gmail.com>
2025-09-06 15:36:14 -0600

    implementar mvc dashboard...?

--- src/src/main/java/Hospital/presentation/dashboard/Controller.java
+++ src/src/main/java/Hospital/presentation/dashboard/Controller.java
@@ -1,4 +1,21 @@
 package Hospital.presentation.dashboard;
 
+import Hospital.logic.Medicamento;
+import Hospital.presentation.medicamentos.Model;
+import Hospital.presentation.medicamentos.View;
+
+import java.util.List;
+
 public class Controller {
+    Model model;
+    View view;
+
+    public Controller(Model model, View view) {
+        this.model = model;
+        this.view = view;
+
+        //view.setController(this);
+        //no se pq chorchas no sirve
+        view.setModel(model);
+    }
 }
--- src/src/main/java/Hospital/presentation/dashboard/Model.java
+++ src/src/main/java/Hospital/presentation/dashboard/Model.java
@@ -1,4 +1,11 @@
 package Hospital.presentation.dashboard;
 
-public class Model {
+import Hospital.logic.Medicamento;
+import Hospital.presentation.AbstractModel;
+
+import java.util.List;
+
+public class Model extends AbstractModel {
+    List<Medicamento> medicamentos;
+
 }
