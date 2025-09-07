ce4dde35151cb82b75c3830c09fd48ab6b605bfb
Aaron-Garc <aarongarfon@gmail.com>
2025-09-07 17:42:26 -0600

    Despacho mvc

--- src/src/main/java/Hospital/presentation/despacho/Model.java
+++ src/src/main/java/Hospital/presentation/despacho/Model.java
@@ -1,4 +1,39 @@
 package Hospital.presentation.despacho;
 
-public class Model {
+import Hospital.logic.recetas.Receta;
+import Hospital.presentation.AbstractModel;
+
+import java.util.List;
+
+public class Model extends AbstractModel {
+    List<Receta> recetas;
+    public static final String RECETAS = "recetas";
+
+    public List<Receta> getRecetasList() {
+        return recetas;
+    }
+
+    public void procesarReceta(int row){
+        Receta r = this.recetas.get(row);
+        r.setEstado("En Proceso");
+        firePropertyChange(RECETAS, null, recetas);
+    }
+
+    public void prepararReceta(int row){
+        Receta r = this.recetas.get(row);
+        r.setEstado("Lista");
+        firePropertyChange(RECETAS, null, recetas);
+    }
+
+    public void despacharReceta(int row){
+        Receta r = this.recetas.get(row);
+        r.setEstado("Entregada");
+        firePropertyChange(RECETAS, null, recetas);
+    }
+
+    public void descartarReceta(int row){
+        Receta r = this.recetas.get(row);
+        recetas.remove(r);
+        firePropertyChange(RECETAS, null, recetas);
+    }
 }
