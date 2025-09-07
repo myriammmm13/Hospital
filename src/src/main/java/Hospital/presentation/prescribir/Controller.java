dca2ad8dc92edd55a40295b0db41cd60aa4496d7
Aaron-Garc <aarongarfon@gmail.com>
2025-09-06 23:47:48 -0600

    eliminar medicamento

--- src/src/main/java/Hospital/presentation/prescribir/Controller.java
+++ src/src/main/java/Hospital/presentation/prescribir/Controller.java
@@ -52,4 +52,8 @@ public class Controller {
     public void agregarPrescripcion(Prescripcion p) {
         model.agregarPrescripcion(p);
     }
+
+    public void borrarPrescripcion(int row){
+        model.borrarPrescripcion(model.getPrescripcionesList().get(row));
+    }
 }
